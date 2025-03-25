8a447f95b62761b1b68fa44aa325babef899f995
HelloXiuXiu <xiuxiuweb@gmail.com>
2025-03-25 21:45:35 +0100

    next.js setup

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '@/app/_styles/homepage.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      Hello world
+    </div>
+  )
+}
