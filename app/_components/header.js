5829408e9e16f80c426c483898792ac3b2161c8f
HelloXiuXiu <xiuxiuweb@gmail.com>
2025-04-26 23:16:43 +0200

    styles set up

--- app/_components/header.js
+++ app/_components/header.js
@@ -14,7 +14,12 @@ export default async function Header() {
       {/* <Link href='/day/000000'>Day 0000-00-00</Link>*/}
       {/* <Link href='/day/20250319'>Day 2025-03-19</Link> */}
       {/* <Link href='/chart'>Chart</Link> */}
-      <Link href='/new' className={s.addButton}>+</Link>
+      <Link href='/new' className={s.addButton}>
+        <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
+          <path d='M12 2V23' stroke='currentColor' strokeWidth='2'/>
+          <path d='M1.5 12.5L22.5 12.5' stroke='currentColor' strokeWidth='2'/>
+        </svg>
+      </Link>
       <div>
       {user ? (
         <UserAvatar user={user} />
