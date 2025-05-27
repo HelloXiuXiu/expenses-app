ff1bdd13ec4b73a77a1f8a3cb7ecfc7f9360268f
HelloXiuXiu <xiuxiuweb@gmail.com>
2025-05-27 22:50:01 +0200

    calendar menu animation ready

--- app/_components/CalendarMenu.js
+++ app/_components/CalendarMenu.js
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { SelectCategoriesPopup } from '@/app/_components/SelectCategoriesPopup'
 import s from '@/app/_styles/_components/CalendarMenu.module.css'
 
@@ -15,31 +15,56 @@ export const CalendarMenu = ({
 }) => {
   const [isOpen, setIsOpen] = useState(false)
   const [isCategoriesOpen, setIsCategoriesOpen] = useState(false)
+  const [menuHeight, setMenuHeight] = useState(0)
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+  }, [])
 
   const categoriesLenth = Object.keys(allCategories).length
   const selectedLength = selectedCategories ? selectedCategories.split(',').length : 0
+  // not using sticky as it blocks next.js cache on history.back()
+  const menuIndentStyle = isOpen ? { height: menuHeight + 'px' } : {}
+  const menuStyle = isOpen ? { maxHeight: menuHeight + 'px', pointerEvents: 'auto' } : {}
 
-  function openPopUp(e) {
+  function openCategories(e) {
     setIsCategoriesOpen(true)
-    window.addEventListener('click', closePopUp, { once: true, capture: true })
+    window.addEventListener('click', closeCategories, { once: true, capture: true })
   }
 
-  function closePopUp(e) {
-    const isButton = e.target.closest('.categories-popup-button')
-    const isOption = e.target.closest('.categories-popup-option')
-    if (!isButton && !isOption) e.stopPropagation()
-    if (e.target.closest('.categories-popup-box') && !isButton) {
-      window.addEventListener('click', closePopUp, { once: true, capture: true })
-      return
+  function closeCategories(e) {
+    // to make 'close on click outside' work
+    // add .categories-popup-box to popup wrapper
+    const isInsidePopup = e.target.closest('.categories-popup-box')
+
+    if (isInsidePopup) {
+      window.addEventListener('click', closeCategories, { once: true, capture: true })
+    } else {
+      e.preventDefault()
+      e.stopPropagation()
+      setIsCategoriesOpen(false)
     }
-    if (isButton) e.target.click()
-    setIsCategoriesOpen(false)
+  }
+
+  function handleResize() {
+    // get an actial menu size to enable smooth animation
+    const menu = document.getElementById('menuContent')
+    const height = menu?.getBoundingClientRect().height || 40
+    setMenuHeight(height)
   }
 
   return (
-    <div className={s.menuBox} style={{ height: isOpen ? '40px' : '0' }}>
-     {isOpen ? (
-        <div className={s.menuContent}>
+    <>
+      {!isOpen && (
+        <div className={s.openTrig} onClick={() => setIsOpen(true)}>[ open stats ]</div>
+      )}
+
+      <div className={s.menuIndent} style={menuIndentStyle}></div>
+
+      <div className={s.menuBox} style={menuStyle}>
+        <div id='menuContent' className={s.menuContent}>
+          <div className={s.leftContent}>
             <div className={s.layoutToggle}>
               <svg width='21' height='14' viewBox='0 0 21 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
                 <path d='M0 1H20.5' stroke='currentColor'/>
@@ -48,34 +73,41 @@ export const CalendarMenu = ({
               </svg>
             </div>
             <div className={s.sums}>
-              <div>
-                <span className={s.sum}>{(+sumWeek).toFixed(2)}</span> [ week ]
+              <div style={{display: 'inline' }}>
+                <span className={s.sum}>{(+sumWeek).toFixed(2)}</span>
+                <span className={s.sumLabel}>[ week ]</span>
               </div>
-              <div>
-                <span className={s.sum}>{(+sumMonth).toFixed(2)}</span> [ month ]
+              <div style={{display: 'inline' }}>
+                <span className={s.sum}>{(+sumMonth).toFixed(2)}</span>
+                <span className={s.sumLabel}>[ month ]</span>
               </div>
             </div>
-            <div className={s.closeTrig} onClick={() => setIsOpen(false)}>[ close ]</div>
-            <div
-              className={s.categories + ' categories-popup-trig'}
-              style={{ opacity: isCategoriesOpen ? '0.5' : '1'}}
-              onClick={openPopUp}
-            >
-              {selectedLength}/{categoriesLenth} categories
-            </div>
-            {isCategoriesOpen && (
-              <SelectCategoriesPopup
-                selectedCategories={selectedCategories}
-                onSetSelectedCategories={onSetSelectedCategories}
-                allCategories={allCategories}
-                showDeleted={showDeleted}
-                setShowDeleted={setShowDeleted}
-              />
-            )}
+          </div>
+          <div className={s.closeTrig} onClick={() => setIsOpen(false)}>[ close ]</div>
+          <div
+            className={s.categories}
+            style={{ opacity: isCategoriesOpen ? '0.5' : '1'}}
+            onClick={openCategories}
+          >
+            {selectedLength}/{categoriesLenth} categories
+          </div>
         </div>
-      ) : (
-        <div className={s.openTrig} onClick={() => setIsOpen(true)}>[ open stats ]</div>  
-      )}
-    </div>
+
+        {isCategoriesOpen && (
+          <div
+            className={s.categoriesBox + ' categories-popup-box'}
+            style={{ top: menuHeight + 40 + 'px'}}
+          >
+            <SelectCategoriesPopup
+              selectedCategories={selectedCategories}
+              onSetSelectedCategories={onSetSelectedCategories}
+              allCategories={allCategories}
+              showDeleted={showDeleted}
+              setShowDeleted={setShowDeleted}
+            />
+          </div>
+        )}
+      </div>
+    </>
   )
 }
--- app/_components/SelectCategoriesPopup.js
+++ app/_components/SelectCategoriesPopup.js
@@ -7,6 +7,7 @@ export const SelectCategoriesPopup = ({
   showDeleted,
   setShowDeleted
 }) => {
+
   function handleToggleCategory(category) {
     onSetSelectedCategories(selected => {
       let newCategories = ''
@@ -22,31 +23,29 @@ export const SelectCategoriesPopup = ({
   }
 
   return (
-    <div className={s.box + ' categories-popup-box'}>
-      <ul className={s.categories}>
-        {Object.keys(allCategories).map(category => (
-          <li key={category} className='categories-popup-option'>
-            <label className={s.category}>
-              <input
-                type='checkbox'
-                checked={!!selectedCategories?.includes(category)}
-                onChange={() => handleToggleCategory(category)}
-              />
-              <span>{category}</span>
-            </label>
-          </li>
-        ))}
-        <li className='categories-popup-option' style={{ marginTop: '20px'}}>
+    <ul className={s.categories}>
+      {Object.keys(allCategories).map(category => (
+        <li key={category}>
           <label className={s.category}>
             <input
               type='checkbox'
-              checked={showDeleted}
-              onChange={() => setShowDeleted(state => !state)}
+              checked={!!selectedCategories?.includes(category)}
+              onChange={() => handleToggleCategory(category)}
             />
-            <span>Show deleted categories</span>
+            <span>{category}</span>
           </label>
         </li>
-      </ul>
-    </div>
+      ))}
+      <li style={{ marginTop: '20px'}}>
+        <label className={s.category}>
+          <input
+            type='checkbox'
+            checked={showDeleted}
+            onChange={() => setShowDeleted(state => !state)}
+          />
+          <span>Show deleted categories</span>
+        </label>
+      </li>
+    </ul>
   )
 }
