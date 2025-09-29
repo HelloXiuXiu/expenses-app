c6df74180e540cdcf645cdb1f51e405a6be15a4c
HelloXiuXiu <xiuxiuweb@gmail.com>
2025-09-29 21:19:30 +0200

    handle no categories and future dates for add expense popup

--- app/_components/AddExpenseForm.js
+++ app/_components/AddExpenseForm.js
@@ -1,10 +1,11 @@
 'use client'
 
 import { useState, useTransition } from 'react'
+import Link from 'next/link'
 import { addExpenseAction } from '@/lib/actions/actions'
 import { Button } from '@/app/_components/ui/Button'
 import { DATE_FORMAT } from '@/app/_config/config'
-import { formatNumericVal, isValidAmount, isValidDate } from '@/utils/validation'
+import { formatNumericVal, isValidAmount, isValidDate, isFuture } from '@/utils/validation'
 import s from '@/app/_styles/_components/AddExpenseForm.module.css'
 
 function handleAmountInput(e) {
@@ -15,13 +16,12 @@ function handleAmountBlur(e) {
   e.target.value = isValidAmount(e.target.value) ? +e.target.value : ''
 }
 
-// TO-DO handle no categories case
 export function AddExpenseForm({ settings }) {
-  const categories = settings.categories
+  const categories = Object.keys(settings.categories)
 
   const [isPending, startTransition] = useTransition()
   const [errorMsg, setErrorMsg] = useState('')
-  const [categoryColor, setCategoryColor] = useState(Object.values(categories)[0])
+  const [categoryColor, setCategoryColor] = useState(settings.categories[categories[0]])
   const [status, setStatus] = useState('')
 
   function handleSubmit(e) {
@@ -32,6 +32,7 @@ export function AddExpenseForm({ settings }) {
     const description = form.elements.description.value
     const category = form.elements.category.value
     const date = form.elements.date.value
+    // TO-DO allow multicurrency
     const currency = settings.currency
 
     if (!amount) {
@@ -46,6 +47,11 @@ export function AddExpenseForm({ settings }) {
 
     if (!isValidDate(date)) {
       setErrorMsg('day should be YYYY-MM-DD')
+      return
+    }
+
+    if (isFuture(date)) {
+      setErrorMsg('day cannot be future')
       return
     }
 
@@ -68,16 +74,32 @@ export function AddExpenseForm({ settings }) {
   }
 
   function handleCategories(e) {
-    setCategoryColor(categories[e.target.value])
+    setCategoryColor(settings.categories[e.target.value])
   }
+
+  if (!categories.length) return (
+    <div className={s.noCategories}>
+      <div  className={s.title}>[  to add an expense create at least one category  ]</div>
+      {window.location.pathname !== '/account/settings' && (
+        <Link href="/account/settings" style={{ marginBottom: '16px' }}>
+          <Button.Middle>Go to Settings</Button.Middle>
+        </Link>
+      )}
+    </div>
+  )
 
   return (
     <>
       <div className={s.title}>[  create new expense  ]</div>
-      <form onSubmit={handleSubmit} className={s.form}>
+      <form
+        onSubmit={handleSubmit}
+        onChange={() => setErrorMsg('')}
+        className={s.form}
+      >
         <input
           type="text"
           inputMode="decimal"
+          autoComplete="off"
           name="amount"
           placeholder="Amount"
           className={s.input}
@@ -87,8 +109,6 @@ export function AddExpenseForm({ settings }) {
         <input
           type="text"
           name="date"
-          // TO-DO test at night
-          // TO-DO validate and show error
           placeholder="Date: YYYY-MM-DD"
           defaultValue={new Date().toLocaleDateString(DATE_FORMAT)}
           className={s.input}
@@ -104,7 +124,7 @@ export function AddExpenseForm({ settings }) {
             className={`${s.input} ${s.select}`}
             onChange={handleCategories}
           >
-            {Object.entries(categories).map(([title]) => (
+            {categories.map((title) => (
               <option key={title} value={title}>
                 {title}
               </option>
@@ -113,16 +133,15 @@ export function AddExpenseForm({ settings }) {
           <div className={s.dot} style={{ backgroundColor: categoryColor || "none" }}></div>
         </div>
         
-        <div className={s.relative} style={{ display: "flex"}}>
+        <div className={s.relative} style={{ display: "flex" }}>
           {errorMsg ? <div className={s.errorMsg}><span></span>{errorMsg}</div> : null}
           <Button.Large type="submit" disabled={isPending} style={{ margin: "45px 16px 16px 16px" }}>
             {isPending ? "adding..." : "+ add expense +"}
           </Button.Large>
         </div>
-
-        {status === "success" && <p className={s.success}>Expense added!</p>}
-        {status === "error" && <p className={s.error}>Something went wrong.</p>}
       </form>
+      {status === "success" && <p className={s.success}>Expense added!</p>}
+      {status === "error" && <p className={s.error}>Something went wrong.</p>}
     </>
   )
 }
